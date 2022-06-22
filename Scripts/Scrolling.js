057673c4bcd7b62202539e6ec66b85102fcb5370
FinalActs <95232227+FinalActs@users.noreply.github.com>
2022-06-22 22:43:46 +0300

    Add files via upload

--- Scripts/Scrolling.js
+++ Scripts/Scrolling.js
@@ -1,5 +1,5 @@
 function scrollToItem(item) {
-    var diff=(item.offsetTop-window.scrollY)/10;
+    var diff=(item.offsetTop-window.scrollY)/120;
     if(!window._lastDiff){
         window._lastDiff = 0;
     }
